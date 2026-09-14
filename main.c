bfd83520c130a4aece9536fc3b49d0eaf54d3182
Isabela Rocha Do Nascimento <isabela.rochadonascimento@gmail.com>
2026-09-14 15:22:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,4 +5,20 @@ int main(){
     constroi_pilha (5, &pilha); //int capacidade, t_pilha *p
     return 0;
     
+    empilha(&pilha, 30);
+    exibirDecimaPrabaixo(&pilha);
+    int desempilhado;
+    desempilha(&pilha, &desempilhado);
+    exibirDecimaPrabaixo(&pilha);
+    desempilhado = extrai_abaixo_do_topo(&pilha);
+    printf("%d estava abaixo do topo\n", desempilhado);
+    exibirDecimaPrabaixo(&pilha);
+    empilha(&pilha, 15);
+    empilha(&pilha, 5);
+    exibirDecimaPrabaixo(&pilha);
+    amassa_topo(&pilha, 10);
+    exibirDecimaPrabaixo(&pilha);
+    amassa_pilha(&pilha, 18);
+    exibirDecimaPrabaixo(&pilha);
+    return 0;
 }
